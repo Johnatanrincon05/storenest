9dcc5d8b41080ef78e101f0109ac1e08e7fb1237
Johnatanrincon05 <59593603+Johnatanrincon05@users.noreply.github.com>
2022-05-22 12:41:43 -0500

    UsersRepository Create Method struturated

--- src/auth/repository/users.repository.ts
+++ src/auth/repository/users.repository.ts
@@ -6,6 +6,10 @@ import { User } from '../entities/user.entity';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   
-  async createUser(registerUserDto:RegisterUserDto):Promise<void> {}
+  async createUser(registerUserDto:RegisterUserDto):Promise<void> {
+    const { name, email, password } = registerUserDto;
+    const user = this.create({ name, email, password });
+    await this.save(user);
+  }
 
 }
