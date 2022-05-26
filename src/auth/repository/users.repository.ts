17fad939e58154e3d0e8a4911a1756ad3190e7e6
Johnatan <Johnatanrincon05>
2022-05-25 23:05:59 -0500

    UserRegister - (UserRepository) Implements BCRYPT for secure password

--- src/auth/repository/users.repository.ts
+++ src/auth/repository/users.repository.ts
@@ -3,13 +3,19 @@ import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { EntityRepository, Repository } from 'typeorm';
 import { RegisterUserDto } from '../dto/register-user.dto';
 import { User } from '../entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
 
   async createUser(registerUserDto: RegisterUserDto): Promise<void> {
     const { name, email, password } = registerUserDto;
-    const user = this.create({ name, email, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ name, email, password: hashedPassword });
+
     try {
       await this.save(user);
     } catch (error) {
