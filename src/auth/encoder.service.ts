e4278f0fa9a082c754f461c9f6d9d8632171247b
Johnatanrincon05 <59593603+Johnatanrincon05@users.noreply.github.com>
2022-05-26 11:30:32 -0500

    userManagement - (UserRepository): Fixed arquitecture, hash password separated from repository to service (encode service)

---
+++ src/auth/encoder.service.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class EncoderService{
+  async encodePassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(password, salt);
+  }
+}
