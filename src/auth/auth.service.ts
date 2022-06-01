05061ed5ba7b1d3ada124240728b0d9e4d3a1692
Johnatan <Johnatanrincon05>
2022-05-31 22:41:46 -0500

    [AuthService&&AuthController]: JwtService Implemented and returned from the controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LoginDto } from './dto/login.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { EncoderService } from './encoder.service';
-import { User } from './entities/user.entity';
+import { JwtPayload } from './jwt-payload.interface';
 import { UsersRepository } from './repository/users.repository';
 
 @Injectable()
@@ -12,20 +13,24 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
     private encoderService: EncoderService,
+    private jwtService: JwtService,
   ) {}
   async registerUser(registerUserDto: RegisterUserDto): Promise<void> {
     const { name, email, password } = registerUserDto;
     const hashedPassword = await this.encoderService.encodePassword(password);
     return this.usersRepository.createUser(name, email, hashedPassword);
   }
-  async login(loginDto: LoginDto): Promise<string> {
+  async login(loginDto: LoginDto): Promise<{accessToken: string}> {
     const { email, password } = loginDto;
     const userDatabase = await this.usersRepository.findOneByEmail(email);
     if (
       userDatabase &&
       (await this.encoderService.checkPassword(password, userDatabase.password))
     ) {
-      return 'Logged';
+      const payload: JwtPayload = { id: userDatabase.id, email, active: userDatabase.active};
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
     }
     throw new UnauthorizedException('Bad Credentials');
   }
