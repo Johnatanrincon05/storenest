9679de703f984e8a739dba750b85c7871dda9a3a
Johnatan <Johnatanrincon05>
2022-05-31 22:31:47 -0500

    [JWT  implementation] - Jwt interface to strategy, jwt strategy and auth module updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,23 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './repository/users.repository';
 import { EncoderService } from './encoder.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret:'prueba',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
+    TypeOrmModule.forFeature([UsersRepository])
+  ],
   controllers: [AuthController],
-  providers: [AuthService, EncoderService],
+  providers: [AuthService, EncoderService, JwtStrategy],
+  exports:[JwtStrategy, PassportModule]
 })
 export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "./entities/user.entity";
+import { JwtPayload } from "./jwt-payload.interface";
+import { UsersRepository } from "./repository/users.repository";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {
+    super({
+      secretOrKey: 'prueba',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+    const user = this.usersRepository.findOneByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
